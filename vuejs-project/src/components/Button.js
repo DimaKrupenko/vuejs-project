d5c571eb6489fe91825b1eccf520f38d4484370b
DimaKrupenko <dimakrupenko77@gmail.com>
2023-06-21 17:58:44 +0300

    upd

--- vuejs-project/src/components/Button.js
+++ vuejs-project/src/components/Button.js
@@ -1,9 +1,19 @@
-import Vue from 'vue';
+// import { Vue } from 'vue';
 
-const Button = Vue.component('Btn-b', {
-  render(createElement) {
-    return createElement('button', this.$slots.default);
+// const Button = Vue.component('Btn-b', {
+//   render(createElement) {
+//     return createElement('button', this.$slots.default);
+//   },
+// });
+
+// export default Button;
+
+import { h } from 'vue';
+
+const Button = {
+  render() {
+    return h('button', this.$slots.default());
   },
-});
+};
 
 export default Button;
