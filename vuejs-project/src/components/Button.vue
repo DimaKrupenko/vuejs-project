95859142b60c63022beb3d42fd171dc2c3ff8a6e
DimaKrupenko <dimakrupenko77@gmail.com>
2023-06-27 13:55:12 +0300

    upd

--- vuejs-project/src/components/Button.vue
+++ vuejs-project/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button v-bind:type="type" >
+    <button v-bind:type="type" class="btn">
 <slot></slot>
     </button>
 </template>
@@ -9,9 +9,19 @@ export default {
     name: 'MainButton',
     props: {
         type: String
-    }
+    },
+    
 }
 </script>
-<style lang="scss" scoped>
-
+<style  scoped>
+.btn{
+display: inline-block;
+font-size: 18px;
+background-color: orange;
+color: white;
+cursor: pointer;
+min-width: 220px;
+border: 1px solid transparent;
+padding: 8px 15px;
+}
 </style>
--- vuejs-project/vue.config.js
+++ vuejs-project/vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// const { defineConfig } = require('@vue/cli-service');
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+module.exports = {
+  chainWebpack: config => {
+    // Добавляем sass-loader для обработки файлов SASS/SCSS
+    config.module
+      .rule('sass')
+      .test(/\.sass$/)
+      .use('sass-loader')
+      .loader('sass-loader')
+      .end();
+
+    config.module
+      .rule('scss')
+      .test(/\.scss$/)
+      .use('sass-loader')
+      .loader('sass-loader')
+      .end();
+  },
+};
